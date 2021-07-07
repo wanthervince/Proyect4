c3e96e29aabb2a88834354436eb7a0479f640039
WanTher-VinCe <lonely.wolf.1594@gmail.com>
2021-07-07 16:35:00 -0500

    generando funcion tirardados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'proyect4';
+  dadoI = '../assets/img/dice1.png';
+  dadoD = '../assets/img/dice2.png';
+
+  n1: number;
+  n2: number;
+
+  constructor() {
+    this.n1 = 1;
+    this.n2 = 1;
+  }
+
+  tirardado(): void {
+    this.n1 = Math.floor(Math.random() * (6 - 1) + 1);
+    this.n2 = Math.floor(Math.random() * (6 - 1) + 1);
+
+    this.dadoI = `../assets/img/dice${this.n1}.png`;
+    this.dadoD = `../assets/img/dice${this.n2}.png`;
+  }
 }
